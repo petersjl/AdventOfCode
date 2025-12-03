85b6e951d5c182a76c75b7518c42ac511e398ece
Joseph Peters <petersjl@rose-hulman.edu>
2025-12-03 01:11:31 -0500

    Update test template for ease of use

--- 2025/test/Day03_test.dart
+++ 2025/test/Day03_test.dart
@@ -9,7 +9,7 @@ void main() {
   if (DAY.isEmpty) {
     throw Exception("Please set the DAY constant to the day being tested.");
   }
-  for (var (file, p1, p2) in [('A', 357, 3121910778619)])
+  for (var (file, p1, p2) in [('A', "357", "3121910778619")])
     group("Check sample input $file passes for part", () {
       late var input;
       setUp(() {
@@ -18,11 +18,11 @@ void main() {
         );
       });
       test("1", () {
-        expect(solvePart1(input), p1.toString());
-      }, skip: p1 == null);
+        expect(solvePart1(input), p1);
+      }, skip: p1.isEmpty);
       test("2", () {
-        expect(solvePart2(input), p2.toString());
-      }, skip: p2 == null);
+        expect(solvePart2(input), p2);
+      }, skip: p2.isEmpty);
     });
 
   group("Check actual input passes for part", () {
--- utils/bin/templates/test.dart
+++ utils/bin/templates/test.dart
@@ -8,7 +8,7 @@ void main() {
   if (DAY.isEmpty) {
     throw Exception("Please set the DAY constant to the day being tested.");
   }
-  for (var (file, p1, p2) in [('A', null, null)])
+  for (var (file, p1, p2) in [('A', "", "")])
     group("Check sample input $file passes for part", () {
       late var input;
       setUp(() {
@@ -18,10 +18,10 @@ void main() {
       });
       test("1", () {
         expect(solvePart1(input), p1.toString());
-      }, skip: p1 == null);
+      }, skip: p1.isEmpty);
       test("2", () {
         expect(solvePart2(input), p2.toString());
-      }, skip: p2 == null);
+      }, skip: p2.isEmpty);
     });
 
   group("Check actual input passes for part", () {
