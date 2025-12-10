6971fde3d5fcf8607a86111ffe3901a9f9290e74
Joseph Peters <petersjl@rose-hulman.edu>
2025-12-09 19:04:49 -0500

    Improve RightPolygon

--- utils/lib/DataStructures/RightPolygon.dart
+++ utils/lib/DataStructures/RightPolygon.dart
@@ -1,32 +1,50 @@
 import 'package:utils/DartUtils.dart';
+import 'package:utils/DataStructures/AxisLine.dart';
 
 class RightPolygon {
   List<Point> points;
+  List<AxisLine> _lines;
   int maxX = 0;
   int maxY = 0;
-  RightPolygon(List<Point> points) : this.points = List.unmodifiable(points) {
+
+  RightPolygon(List<Point> points)
+    : this.points = List.unmodifiable(points),
+      _lines = [] {
+    for (int i = 0; i < points.length; i++) {
+      final a = points[i];
+      final b = points[(i + 1) % points.length];
+      if (a.x > maxX) maxX = a.x;
+      if (a.y > maxY) maxY = a.y;
+      _lines.add(AxisLine(a, b));
+    }
     for (final p in points) {
       if (p.x > maxX) maxX = p.x;
       if (p.y > maxY) maxY = p.y;
     }
   }
 
-  Iterable<(Point, Point)> edges() sync* {
-    for (int i = 0; i < points.length; i++) {
-      final a = points[i];
-      final b = points[(i + 1) % points.length];
-      yield (a, b);
-    }
+  List<AxisLine> edges() {
+    return List.unmodifiable(_lines);
   }
 
-  bool isInside(Point p) {
-    var inside = false;
-    for (var (a, b) in edges()) {
-      if (((a.y > p.y) != (b.y > p.y)) &&
-          (p.x < (b.x - a.x) * (p.y - a.y) / (b.y - a.y) + a.x)) {
-        inside = !inside;
+  bool containsPoint(Point p) {
+    var ray = AxisLine(Point(p.x, maxY + 1), p);
+    var collisions = 0;
+    for (var line in edges()) {
+      if (ray.crosses(line)) collisions++;
+    }
+    return collisions % 2 == 1;
+  }
+
+  bool contains(RightPolygon other) {
+    for (var point in other.points) {
+      if (!containsPoint(point)) return false;
+    }
+    for (var myEdge in this.edges()) {
+      for (var theirEdge in other.edges()) {
+        if (myEdge.crosses(theirEdge)) return false;
       }
     }
-    return inside;
+    return true;
   }
 }
