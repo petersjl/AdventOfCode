05de8f4a77f19e73083c330cbe0dc5d1f81b21ed
Joseph Peters <petersjl@rose-hulman.edu>
2025-12-12 00:07:03 -0500

    Setup Day12

---
+++ 2025/bin/Day12.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: dead_code
+
+import 'package:utils/DartUtils.dart';
+
+void main() {
+  var rawInput = Utils.readToString("../inputs/Day12.txt");
+  Utils.runWithTiming(parseInput, solvePart1, solvePart2, rawInput);
+}
+
+typedef InputType = List<String>;
+
+InputType parseInput(String input) {
+  return input.splitNewLine();
+}
+
+String solvePart1(InputType input) {
+  return "";
+}
+
+String solvePart2(InputType input) {
+  return "";
+}
