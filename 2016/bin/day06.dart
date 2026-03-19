9d944a31fdf8e96b8086fa5026c044205be77165
Joseph Peters <petersjl@rose-hulman.edu>
2026-03-19 13:00:33 -0400

    2016 06 2

--- 2016/bin/day06.dart
+++ 2016/bin/day06.dart
@@ -17,21 +17,33 @@ String solvePart1(InputType input) {
   int runs = input[0].length;
   String message = '';
   for (int i = 0; i < runs; i++) {
-    message += findMostCommonAtPosition(input, i);
+    message += findCommonAtPosition(input, i);
   }
   return message;
 }
 
 String solvePart2(InputType input) {
-  return "";
+  int runs = input[0].length;
+  String message = '';
+  for (int i = 0; i < runs; i++) {
+    message += findCommonAtPosition(input, i, true);
+  }
+  return message;
 }
 
-String findMostCommonAtPosition(List<String> input, int index) {
+String findCommonAtPosition(
+  List<String> input,
+  int index, [
+  bool least = false,
+]) {
   Map<String, int> map = {};
   for (String s in input) {
     String char = s[index];
     map.increment(char);
   }
-  var pairs = map.entries.toList()..sort((a, b) => b.value.compareTo(a.value));
+  var pairs = map.entries.toList()
+    ..sort(
+      (a, b) => least ? a.value.compareTo(b.value) : b.value.compareTo(a.value),
+    );
   return pairs.first.key;
 }
