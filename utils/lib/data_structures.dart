19526dd458de26d40b6c4375b648cf7c57ac98e9
Joseph Peters <petersjl@rose-hulman.edu>
2026-03-23 17:14:28 -0400

    Fix data structures outputs

--- utils/lib/data_structures.dart
+++ utils/lib/data_structures.dart
@@ -2,7 +2,7 @@ export 'data_structures/axis_line.dart' show AxisLine;
 export 'data_structures/grid.dart' show Grid;
 export 'data_structures/growables.dart' show GrowableGrid, GrowableList;
 export 'data_structures/linear_collections.dart'
-    show Queue, Stack, PriorityQueue;
+    show Queue, Stack, PriorityQueue, LinkedList, LinkedRing, Binode;
 export 'data_structures/right_polygon.dart' show RightPolygon;
 export 'data_structures/union_find.dart' show UnionFind;
 export 'data_structures/union_find_int.dart' show UnionFindInt;
