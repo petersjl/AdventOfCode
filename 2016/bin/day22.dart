e5176a59f79affe98425cda7e43e46e57c4b608e
Joseph Peters <petersjl@rose-hulman.edu>
2026-03-24 14:12:59 -0400

    2016 22 2

--- 2016/bin/day22.dart
+++ 2016/bin/day22.dart
@@ -1,6 +1,10 @@
 // ignore_for_file: dead_code
 
+import 'dart:math';
+
+import 'package:utils/algorithms.dart';
 import 'package:utils/dart_utils.dart';
+import 'package:utils/data_structures.dart';
 
 void main() {
   var rawInput = Utils.readToString("../inputs/day22.txt");
@@ -41,7 +45,50 @@ String solvePart1(InputType input) {
 }
 
 String solvePart2(InputType input) {
-  return "";
+  int maxX = 0;
+  int maxY = 0;
+  DataNode? emptyNode;
+  // Find the dimensions of the grid and the empty node
+  for (var node in input) {
+    if (node.y == 0) {
+      maxX = max(maxX, node.x);
+    }
+    if (node.x == 0) {
+      maxY = max(maxY, node.y);
+    }
+    if (node.used == 0) {
+      emptyNode = node;
+    }
+  }
+  if (emptyNode == null) {
+    throw Exception("No empty node found");
+  }
+  var walls = Grid<bool>((x, y) => false, maxX + 1, maxY + 1);
+  for (var node in input) {
+    if (node.used > 200) {
+      walls.set(node.x, node.y, true);
+    }
+  }
+  // Put a hole right in front of the goal data, then move it left until it's in the top left corner
+  Point holeDestination = Point(maxX - 1, 0);
+  // Get the steps to place the hole while dodging walls
+  List<Point>? toHole = aStar(
+    walls,
+    Point(emptyNode.x, emptyNode.y),
+    holeDestination,
+    (a) => Utils.ManhattanDist(a, holeDestination),
+  );
+  // If there is no path to the hole, then there is no solution
+  if (toHole == null) {
+    throw Exception("No path found to hole destination");
+  }
+  int steps = toHole.length - 1;
+  // Move the goal data into the hole
+  steps++;
+  // Move the goal data left until it's in the top left corner
+  // which takes 5 steps per move of the goal data
+  steps += 5 * (maxX - 1);
+  return steps.toString();
 }
 
 class DataNode {
--- 2016/test/day22_test.dart
+++ 2016/test/day22_test.dart
@@ -9,7 +9,7 @@ void main() {
   if (DAY.isEmpty) {
     throw Exception("Please set the DAY constant to the day being tested.");
   }
-  for (var (file, p1, p2) in [('A', "7", "")])
+  for (var (file, p1, p2) in [('A', "7", "7")])
     group("Check sample input $file passes for part", () {
       late var input;
       setUp(() {
@@ -31,7 +31,7 @@ void main() {
       input = parseInput(Utils.readToString('../inputs/day$DAY.txt'));
     });
     const part1Answer = "960";
-    const part2Answer = "";
+    const part2Answer = "225";
     test("1", () {
       expect(solvePart1(input), part1Answer);
     }, skip: part1Answer.isEmpty);
