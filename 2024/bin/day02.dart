b32530332b3886581b86d1734faf15ca377237a6
Joseph Peters <petersjl@rose-hulman.edu>
2026-04-16 13:37:20 -0400

    Fix input imports and add times for 2024

--- 2024/bin/day02.dart
+++ 2024/bin/day02.dart
@@ -3,7 +3,7 @@
 import 'package:utils/dart_utils.dart';
 
 void main() {
-  var rawInput = Utils.readToString("../inputs/day01.txt");
+  var rawInput = Utils.readToString("../inputs/day02.txt");
   Utils.runWithTiming(parseInput, solvePart1, solvePart2, rawInput);
 }
 
